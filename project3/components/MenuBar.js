fdf884f83f5fd250eb79d9d11e7af7992ca61187
svenngronbeck <srgronbe@stud.ntnu.no>
2018-10-02 13:25:37 +0200

    Created menu bar with images. #6

--- project3/components/MenuBar.js
+++ project3/components/MenuBar.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ListItem from "./ListItem.js";
 import styles from "../stylesheets/MenuBar.style.js";
+import MenuItem from "./MenuItem.js";
+import HomeIcon from "../assets/home.svg";
 
 export default class MenuBar extends React.Component {
     render() {
         return (
             <View style={styles.menuBar}>
-                <Text>legg til menyer her</Text>
+                <MenuItem source={require('../assets/home.png')}/>
+                <MenuItem source={require('../assets/list.png')}/>
+                <MenuItem source={require('../assets/home.png')}/>
+                <MenuItem source={require('../assets/home.png')}/>
+                <MenuItem source={require('../assets/home.png')}/>
             </View>
         );
     }
