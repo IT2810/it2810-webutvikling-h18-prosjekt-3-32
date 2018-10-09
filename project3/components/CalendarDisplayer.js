9395c2120b1015dcf00edbe8be3f13d27b8a7884
svenngronbeck <srgronbe@stud.ntnu.no>
2018-10-09 17:56:59 +0200

    Made som more changes to the UI, and implemented a modal for adding new events. #23

--- project3/components/CalendarDisplayer.js
+++ project3/components/CalendarDisplayer.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, List } from 'react-native';
+import { StyleSheet, Text, View, List, TouchableOpacity, Modal, Image, TextInput } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import styles from "../stylesheets/Calendar.style.js";
+import DatePicker from 'react-native-datepicker';
 
 export default class CalendarDisplayer extends React.Component {
     constructor(props) {
       super(props);
-      var today = new Date(),
-            date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
       this.state = {
-        date: date,
-        items: {},
+        date: "",
+        time: "",
+        modalVisible: false,
+        items: {'2018-10-09': [{name: 'Finish calendar page on this app', time: '16:00-18:00'}],
+                 '2018-10-11': [{name: 'Finish assignment 2 in IR', time: '10:00-16:00'}],
+                 '2018-10-12': [{name: 'Forelesning', time: '12:15-14:00'},{name: 'Fylla', time: '18:00-23:59'}],
+                },
       };
     }
 
+
     render() {
         return (
+          <View style={styles.calendarContainer}>
             <Agenda
               style = {styles.agendaContainer}
+              //Set monday to be first day of week
+              firstDay={1}
+              //Show week numbers
+              showWeekNumbers={true}
               items={this.state.items}
               // callback that gets called when items for a certain month should be loaded (month became visible)
               loadItemsForMonth={this.loadItems.bind(this)}
@@ -26,18 +37,6 @@ export default class CalendarDisplayer extends React.Component {
               onDayPress={(day)=>{console.log('day pressed')}}
               // callback that gets called when day changes while scrolling agenda list
               onDayChange={(day)=>{console.log('day changed')}}
-              /*
-              // initially selected day
-              selected={'2018-10-08'}
-              // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-              minDate={'2012-05-10'}
-              // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-              maxDate={'2022-05-30'}
-              // Max amount of months allowed to scroll to the past. Default = 50
-              pastScrollRange={50}
-              // Max amount of months allowed to scroll to the future. Default = 50
-              futureScrollRange={50}
-              */
               // specify how each item should be rendered in agenda
               renderItem={this.renderItem.bind(this)}
               // specify how empty date content with no items should be rendered
@@ -45,40 +44,132 @@ export default class CalendarDisplayer extends React.Component {
               // specify your item comparison function for increased performance
               rowHasChanged={this.rowHasChanged.bind(this)}
             />
+            <TouchableOpacity style={styles.addEventButton} onPress={() => this.setModalVisible(true)} >
+              <Text style={styles.addEventText}>{"Add new event"}</Text>
+            </TouchableOpacity>
+            <View>
+                {/*Modal which contains several options to the user regarding the todo*/}
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}>
+                    <View style={styles.modal}>
+                        {/*Simple backbutton if the user choose to not do any changes*/}
+                        <View style={styles.newEventTitleBar}>
+                          <TouchableOpacity
+                              onPress={() => {this.setModalVisible(!this.state.modalVisible);}}
+                              style={styles.modalClose}>
+                              <Image style={styles.backBtn} source={require('../assets/back.png')}/>
+                          </TouchableOpacity>
+                          <Text style={styles.newEventTitle}>{"Add a new event"}</Text>
+                        </View>
+                        <TextInput
+                            style={styles.textInput}
+                            onChangeText={(text) => this.setState({newEventText : text})}
+                            placeholder={"Event description"}
+                        />
+
+                        <View style={styles.modalItem}>
+                            {/*Date picker so the user could choose an due date to the todo.*/}
+                            <DatePicker
+                                date={this.state.date}
+                                mode="date"
+                                placeholder="select date"
+                                minDate={new Date()}
+                                format="YYYY-MM-DD"
+                                confirmBtnText="Confirm"
+                                cancelBtnText="Cancel"
+                                onDateChange={(date) => {this.setState({date: date})}}
+                            />
+                        </View>
+                        <View style={styles.modalItem}>
+                            {/*Date picker so the user could choose an due date to the todo.*/}
+                            <DatePicker
+                                date={this.state.date}
+                                mode="time"
+                                placeholder="select time"
+                                format="HH:MM"
+                                confirmBtnText="Confirm"
+                                cancelBtnText="Cancel"
+                                onTimeChange={(time) => {this.setState({time: time})}}
+                            />
+                        </View>
+                        <View style={styles.modalItem}>
+                            {/*Button to save the changes done in the modal*/}
+                            <TouchableOpacity style={styles.saveTodo}
+                                              onPress={() => {this.setModalVisible(!this.state.modalVisible);}}>
+                                <Text style={styles.saveText}>Save</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.modalItem}>
+                            {/*Button to delete the todo*/}
+                            <TouchableOpacity style={styles.deleteTodo} >
+                                <Text style={styles.deleteText}>Delete</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
+            </View>
+          </View>
 
         );
     }
 
+    //Set the modal visible
+    setModalVisible(visible) {
+      this.setState({modalVisible: visible});
+    }
+
+    addEvent(){
+      alert("Add new event");
+    }
+
     loadItems(day) {
       setTimeout(() => {
+        //Run this loop 99 times
         for (let i = -15; i < 85; i++) {
+          //Getting "today's" date. By today I mean the relevant date,
+          //i.e the one we're looping through. When i=0, that is the current date
           const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+          //Converting it to a string
           const strTime = this.timeToString(time);
+          //If there is no object in this.state.items named today's date
           if (!this.state.items[strTime]) {
+            //Then make that object
             this.state.items[strTime] = [];
-            const numItems = Math.floor(Math.random() * 5);
-            for (let j = 0; j < numItems; j++) {
-              this.state.items[strTime].push({
-                name: 'Item for ' + strTime,
-                height: Math.max(50, Math.floor(Math.random() * 150))
-              });
-            }
+            //Add item to the object which tells us that there are no plans on today's agenda
+            this.state.items[strTime].push({
+              name: 'No plans for ' + strTime,
+            });
           }
         }
         //console.log(this.state.items);
+        //Making a new, empty object to add the newItems in
         const newItems = {};
+        //Get every key (every date) in this.state.items and for each key, get item items in the state-list, and add it to newItems
         Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+        //Sets state to be the newItems list.
         this.setState({
-          items: newItems
+          items: newItems,
         });
+        console.log("Today is: ", this.state.date);
       }, 1000);
       // console.log(`Load Items for ${day.year}-${day.month}`);
     }
 
     renderItem(item) {
+      const timeString = item.time;
+
       return (
-        <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+        <TouchableOpacity style={styles.item} onPress={this.showItemInfo.bind(item.name)}>
+          <Text style={styles.itemTime}>{item.time}</Text>
+          <Text style={styles.itemText} >{item.name}</Text>
+        </TouchableOpacity>
       );
+    }
+
+    showItemInfo(itemName){
+      alert("Showing item" + itemName);
     }
 
     renderEmptyDate() {
@@ -96,24 +187,3 @@ export default class CalendarDisplayer extends React.Component {
       return date.toISOString().split('T')[0];
     }
   }
-
-  const styles = StyleSheet.create({
-    item: {
-      backgroundColor: 'white',
-      flex: 1,
-      borderRadius: 5,
-      padding: 10,
-      marginRight: 10,
-      marginTop: 17
-    },
-    emptyDate: {
-      height: 15,
-      flex:1,
-      paddingTop: 30
-    },
-    agendaContainer: {
-      width: "100%",
-      height: "80%",
-    }
-
-});
--- project3/stylesheets/Calendar.style.js
+++ project3/stylesheets/Calendar.style.js
@@ -3,10 +3,145 @@ import {StyleSheet} from "react-native";
 export default StyleSheet.create({
     calendarContainer: {
       width: "100%",
-      height: "90%",
+      height: "100%",
+    },
+    agendaContainer: {
+      height: "80%"
+    },
+    addEventButton: {
+      position: "absolute",
+      width: 70,
+      height: 70,
+      top: "80%",
+      left: "75%",
+      backgroundColor: "#5cacec",
+      justifyContent: "center",
+      borderRadius: 100,
+      shadowOffset: { width: 1, height: 1, }, //IOS
+      shadowColor: "black", //IOS
+      shadowOpacity: 1.0, //IOS
+      shadowRadius: 2, //IOS
+      elevation: 4, //Android shadow
+    },
+    addEventText: {
+      textAlign:'center',
+      color: "white",
+    },
+    item: {
+      backgroundColor: 'white',
+      flex: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      marginTop: 17
+    },
+    itemText: {
+      fontSize: 17,
+      color: "#a0a0a0",
+    },
+    itemTime: {
+      fontSize: 15,
+      color: "#a0a0a0",
+    },
+    emptyDate: {
+      height: 15,
+      flex:1,
+      paddingTop: 30
     },
     agendaContainer: {
       width: "100%",
       height: "80%",
+    },
+    deleteTodo: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#ff3d3d",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    deleteText: {
+        fontSize: 16,
+        color: "#f7f7f7",
+        fontWeight: "bold",
+    },
+
+    saveTodo: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#3aadff",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    saveText: {
+        fontSize: 16,
+        color: "#f7f7f7",
+        fontWeight: "bold",
+    },
+
+    todoInfo:{
+        height: 30,
+        width: 30,
+        marginRight: 20,
+    },
+
+    modalClose: {
+        alignSelf: "flex-start",
+        marginLeft: 10,
+        marginBottom: 15,
+    },
+
+    modalText: {
+        width: "100%",
+        fontSize: 25,
+        textAlign: "center",
+        backgroundColor: "#dddddd",
+        padding: 10,
+    },
+
+    modal: {
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        display: "flex",
+        justifyContent: "flex-start",
+        marginTop: 0,
+    },
+
+    modalItem: {
+        width: "100%",
+        height: "15%",
+        backgroundColor: "#ededed",
+        borderBottomWidth: 0.2,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+    },
+    textInput: {
+        width: "80%",
+        height: "12%",
+        color: "#a0a0a0",
+        fontSize: 18,
+    },
+    newEventTitle: {
+        //width: "80%",
+        height: "100%",
+        fontSize: 20,
+        backgroundColor: "lightgrey",
+        textAlign: "center",
+        flex: 8,
+    },
+    backBtn: {
+        height: "100%",
+        flex: 2,
+        marginTop: 5,
+    },
+    newEventTitleBar: {
+      display: "flex",
+      flexDirection: "row",
+      height: "7.5%",
+      alignItems: "center",
+      justifyContent: "center",
     }
 });
